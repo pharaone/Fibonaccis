5ac8d119c2424962e73113232485851f9701501d
Emanuele Antonio Faraone <emanueleant03@gmail.com>
2017-09-23 20:58:01 +0200

    Fibonaccis 0.0.2 beta

--- Fibonaccis.c
+++ Fibonaccis.c
@@ -1,6 +1,6 @@
 //
 //  main.c
-//  Fibonaccis
+//  Fibonaccis 0.0.2
 //
 //  Created by Emanuele Antonio Faraone on 23/09/17.
 //  Copyright © 2017 Emanuele Antonio Faraone. All rights reserved.
@@ -9,8 +9,10 @@
 int main() {
     int prec1 = 1; // first number
     int prec2 = 1; // second number
-    int r , i ; // r: result
-    for (i= 0 ; i < 44 ; i += 1){
+    int r , i, howmanyt  = 0 ; // r: result, howmanyt : how many times to do the fibonacci
+    printf("How many times do I have to do the Fibonacci?\n");
+    scanf("%d" ,&howmanyt);
+    for (i = 0; i <= howmanyt; i++){
         r = prec1 + prec2;
         printf("\n" "%d", r);
         prec1 = prec2 ;
