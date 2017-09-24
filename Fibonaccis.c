7d22992344ff140bc71555848e6ff51e55918b36
Emanuele Antonio Faraone <emanueleant03@gmail.com>
2017-09-24 08:58:42 +0200

    version 0.0.3 beta
    workaround for #1, this doesn`t solve it

--- Fibonaccis.c
+++ Fibonaccis.c
@@ -1,19 +1,22 @@
 //
 //  main.c
-//  Fibonaccis 0.0.2
+//  Fibonaccis 0.0.3 BETA
 //
 //  Created by Emanuele Antonio Faraone on 23/09/17.
 //  Copyright © 2017 Emanuele Antonio Faraone. All rights reserved.
 //
 #include <stdio.h>
 int main() {
-    int prec1 = 1; // first number
+	int prec1 = 1; // first number
     int prec2 = 0; // second number
     int r , i, howmanyt  = 0 ; // r: result, howmanyt : how many times to do the fibonacci
     printf("How many times do I have to do the Fibonacci?\n");
     scanf("%d" ,&howmanyt);
     if (howmanyt < 1){
     	printf("You can`t set 0 or a negative number, please write a new number: ");
+    	scanf("%d",&howmanyt);
+	}else if (howmanyt > 45){
+		printf("For now Fibonaccis doesn`t support more than 45 fibonaccis, please write a new number: ");
     	scanf("%d",&howmanyt);
 	}
     for (i = 0; i <= howmanyt; i++){
